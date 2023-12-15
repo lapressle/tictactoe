113125f573c9bca64811833f4eb122ea384f4311
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-14 22:19:14 -0500

    Change make_row to avoid poor choices

--- tictactoe.rb
+++ tictactoe.rb
@@ -13,6 +13,29 @@ class GameRows
   def make_row
     "#{left} | #{middle} | #{right}"
   end
+
+  def row_win?
+    left == middle && left == right
+    true
+  end
+
+  def column_win(row2, row3)
+    if left == row2.left && left == row3.left
+      true
+    elsif middle == row2.middle && middle == row3.middle
+      true
+    elsif right == row2.right && right == row3.right
+      true
+    end
+  end
+
+  def cross_win(row2, row3)
+    if left == row2.middle && left == row3.right
+      true
+    elsif right == row2.middle && right == row3.left
+      true
+    end
+  end
 end
 
 # sets up the board game
@@ -40,10 +63,15 @@ class TicTacToe
     gets.chomp
   end
 
+  def check_winner(rows, player)
+    puts rows, player
+  end
+
   def run_game
     while winner == false
       players.each do |player|
-        make_move(input('Pick a spot'), player)
+        make_move(input('Pick a spot: '), player)
+        check_winner(rows, player)
         puts declare_winner if winner != false
         show_board
       end
@@ -52,7 +80,23 @@ class TicTacToe
 
   def declare_winner; end
 
+  def rows_array
+    indexed_rows = []
+    rows.map do |row|
+      row.instance_variables.each do |location|
+        indexed_rows.push(row.instance_variable_get(location))
+      end
+    end
+    indexed_rows
+  end
+
+  def check_false_input(choice)
+    choice = input('Try again: ') while rows_array[choice.to_f - 1] == 'x' || rows_array[choice.to_f - 1] == 'o'
+    choice
+  end
+
   def make_move(choice, player)
+    choice = check_false_input(choice)
     rows.each do |row|
       row.instance_variables.each do |location|
         if row.instance_variable_get(location) == choice.to_s
@@ -102,3 +146,4 @@ game.show_board
 # game.make_move(game.input('Pick a spot'), player_one)
 game.make_move(1, player_one)
 game.show_board
+game.make_move(1, player_two)
