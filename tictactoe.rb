2dbce5e99f08dad6d6dc703a56535fbe8bbf51bb
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-14 19:16:15 -0500

    Set up the initial classes

---
+++ tictactoe.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# each row of tictactoe
+class GameRows
+  attr_accessor :left, :middle, :right
+
+  def initialize(left, middle, right)
+    @left = left
+    @middle = middle
+    @right = right
+  end
+end
+
+# sets up the board game
+class TicTacToe
+  def initialize; end
+end
+
+# set up the players
+class Players
+  def initialize; end
+end
